115bc2caa136e89e36d2dbc61e851bf1045feed3
Marianusf <marianusjr7@gmail.com>
2025-05-24 01:33:57 +0700

    modifikasik kode agar sempurna dengan menambahkan comment dan beberapa detail kecilnya

--- src/Code/teksProcessing.java
+++ src/Code/teksProcessing.java
@@ -4,30 +4,33 @@
  */
 package Code;
 
-import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.analysis.TokenStream;
-import org.apache.lucene.analysis.id.IndonesianAnalyzer;
-import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
-import java.io.StringReader;
-import java.util.ArrayList;
+import org.apache.lucene.analysis.Analyzer;//digunakan untuk semua analyzer Lucene
+import org.apache.lucene.analysis.TokenStream;//token untuk hasil proses analisis teks
+import org.apache.lucene.analysis.id.IndonesianAnalyzer;//analyzer bawaan lucene versi bahasa Indonesia untuk steaming dan stopword
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;//akses untuk token dalam bentuk teks
+import java.io.StringReader;//membaca file dengan asumsi seperti file
+import java.util.ArrayList;//untuk mengembalikan dan menyimpan daftar kata hasil tokenisasi
 import java.util.List;
 /**
  *
  * @author maria
  */
 public class teksProcessing {
-    private final Analyzer analyzer;
+    private final Analyzer analyzer;//analyzer adalah variabel instan dari tipe Analyzer untuk analisis teks
 
     public teksProcessing() {
-        this.analyzer = new IndonesianAnalyzer();
+        this.analyzer = new IndonesianAnalyzer();//agar menggunakan analyzer bahasa Indonesia
     }
 
     public List<String> proses(String text) {
-        List<String> hasil = new ArrayList<>();
-        try (TokenStream tokenStream = analyzer.tokenStream(null, new StringReader(text))) {
-            CharTermAttribute attr = tokenStream.addAttribute(CharTermAttribute.class);
-            tokenStream.reset();
-            while (tokenStream.incrementToken()) {
+        List<String> hasil = new ArrayList<>();//buat arraylist kosong untuk hasil tokenisasi
+        
+        
+        try (TokenStream tokenStream = analyzer.tokenStream(null, new StringReader(text))) {//Menganalisis teks dan StringReader agar analyzer bisa membaca string sebagai input.
+            CharTermAttribute attr = tokenStream.addAttribute(CharTermAttribute.class);//untuk menyimpan kata pada attr CharTermAtrribute
+            tokenStream.reset();//reset untuk digunakan membaca kata/token
+            
+            while (tokenStream.incrementToken()) {//selama ada token berikutnya, maka ambil dan tambahkan ke list hasil
                 hasil.add(attr.toString());
             }
             tokenStream.end();
